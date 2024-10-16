d72660fd7154b70cda96882cde5281964640410b
Austin Moore <119389763+ALmoOorE@users.noreply.github.com>
2024-10-16 14:37:24 +0000

    done

--- src/01/random.cpp
+++ src/01/random.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
+#include<algorithm>
+//#include<string>
+//#include<cctype>
 using namespace std;
 
-int main(){
 
-    return 0;
+bool maybe_pal(string str){
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    string rev = str;
+    reverse(str.begin(), str.end());
+    return rev == str; 
+}
+int main(){
+string s;
+cout<<"Enter a string: ";
+getline(cin,s);
+cout << "\n" << s << (maybe_pal(s) ? " is a pal" : "is not a pal");
+//cout << "\n\"" << s << (maybe_pal(s) ? "\" is" : "\" is not" ) << " a palindrome.\n\n";
+
+
+
+
+return 0;
 }
